dee1ba05dc4d656d7015dfc981d6b9d0906cab2b
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-11-18 16:31:43 -0500

    Update giphy.js

--- giphy.js
+++ giphy.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
-			data = JSON.parse(request.responseText).data.image_url;
+			data = JSON.parse(request.responseText).data.images.fixed_width.url;
 			console.log(data);
 			document.getElementById("giphyme").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
 		} else {
