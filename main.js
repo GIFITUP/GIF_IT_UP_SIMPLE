772d67f315fd221c762c2ffc30b6a35f672a3521
GIFITUP <43257995+GIFITUP@users.noreply.github.com>
2018-11-18 12:43:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
    // httpGetAsync('http://api.giphy.com/v1/gifs/random?' + params, function(data) {
       httpGetAsync('http://api.giphy.com/v1/gifs/random?api_key=EOvxPf7jMMtXmpW6GVGndluHTFeJdFd6', function(data) {
       var gifs = JSON.parse(data);
-      var firstgif = gifs.data[0].images.original.url;
+      var firstgif = gifs.data[0].images.fixed_width.url;
       $("#image").html("<img src='" + firstgif + "'>");
       console.log(gifs.data);
     });
